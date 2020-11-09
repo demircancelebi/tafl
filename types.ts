7dc440988eba175e792a58cc0612be25498e1a35
Demircan Celebi <demircancelebi@gmail.com>
2020-11-09 03:46:48 +0300

    remove unused bits

--- types.ts
+++ types.ts
@@ -45,38 +45,6 @@ interface GameState {
   rules: { [key: string]: Rule }
 }
 
-// class Room {
-//   players?: Map<String, Player>
-//   game?: Game
-
-//   constructor() {}
-
-//   chooseGame(game: Game) {
-//     this.game = game
-//   }
-
-//   addPlayer(index: String, p: Player) {
-//     this.players.set(index, p);
-//   }
-
-//   removePlayer(index: String): boolean {
-//     return this.players.delete(index);
-//   }
-// }
-
-// enum Acts {
-//   CHOOSE_GAME = "CHOOSE_GAME",
-//   GET_ACTIONS = "GET_ACTIONS",
-//   ADD_PLAYER = "ADD_PLAYER",
-//   SET_PLAYER_COUNT = "SET_PLAYER_COUNT",
-//   REMOVE_PLAYER = "REMOVE_PLAYER",
-//   USE_BOARD = "USE_BOARD",
-//   USE_RULESET = "USE_RULESET",
-//   USE_RULE = "USE_RULE",
-//   ACT = "ACT",
-//   FINISHED = "FINISHED"
-// }
-
 class Side extends String {}
 class Rule {}
 class RuleSet {}
